321a2fcb38f61454a79e2fe2a5019a577a221929
pinkkuuuuu <115294297+pinkkuuuuu@users.noreply.github.com>
2022-11-02 19:04:07 +0530

    Create datatype and value type.sol

---
+++ datatype and value type.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+ // DATA TYPES - VALUE & REFERNCES
+
+ contract valueType{
+     bool public boolTemp= true;
+
+     uint public temp=156;
+
+     int public temp2=-134;
+
+     address public adrr=0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+
+     bytes32 public b3;
+ }
